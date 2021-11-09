c547387336292f7754a780af65c1ca689b52cc1e
Franck Kabasele <kabasele467@gmail.com>
2021-11-09 18:52:42 -0500

    use memoization to avoid rerender

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -14,13 +15,14 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
   </div>
 );
 
+
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({ cart: {cartItems }}) => ({
-  itemCount: cartItems.reduce((accumulatedQuantity, cartItem) =>  accumulatedQuantity + cartItem.quantity, 0)
-})
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
 
 export default connect(
   mapStateToProps,

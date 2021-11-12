1eb40326a2894f7673e3b732edab4a16fe63bd1a
Franck Kabasele <kabasele467@gmail.com>
2021-11-12 01:50:34 -0500

    add stripe component

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51JutG4I3likyCmd5CVri2S0t4bkaX8siAbFy3cBwKWtpUXwPcRAk3QLNamOvULPfdPj8XqFgbY9Gb8UYWg603e4T00vLhhiAmS';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Kena-Boutique Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
